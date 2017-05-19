75ff0fc5deb8f48e1111678f266fe022c487c227
Anthony Manetti <tmanetti1992@gmail.com>
2017-05-19 10:27:50 -0500

    init commit

---
+++ src/main/java/com/example/tony/firstapp/G_ControlClient.java
@@ -0,0 +1,39 @@
+package com.example.tony.firstapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Tony on 5/18/2017.
+ */
+
+public class G_ControlClient {
+    private static final String api_key = "9482fbe6126e13d94a463153c62cd857";
+    private static final String api_base = "http://api.openweathermap.org/data/2.5/weather?";
+    private static ArrayList<G_DelegateClient> zip_controllers;
+
+    public ArrayList<G_DelegateClient> delegates() {
+        return zip_controllers;
+    }
+
+    public String apiBaseUrl() {
+        return api_base;
+    }
+
+    public void addZipDelegate(int zipcode) {
+        G_DelegateClient newclient = new G_DelegateClient(zipcode);
+        newclient.args().put("appid",api_key);
+        zip_controllers.add(newclient);
+    }
+
+    private static G_ControlClient instance = null;
+
+    public static G_ControlClient instance() {
+        if (instance == null)
+            instance = new G_ControlClient();
+        return instance;
+    }
+
+    private G_ControlClient() {
+        zip_controllers = new ArrayList<G_DelegateClient>();
+    }
+}
